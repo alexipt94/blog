654eacbbb9035115d10e0f305829a409d22beecc
Alexey Iptyshev <hoeati1@gmail.com>
2023-04-03 12:29:42 +0700

    77

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -7,14 +7,30 @@ describe('classNames', () => {
 
     test('additional', () => {
         const expected = 'someClass class1 class2 hovered scrollable';
-        expect(classNames('someClass', { hovered: true, scrollable: true }, ['class1', 'class2'])).toBe(expected);
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: true },
+
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
     });
     test('additional2', () => {
         const expected = 'someClass class1 class2 hovered';
-        expect(classNames('someClass', { hovered: true, scrollable: false }, ['class1', 'class2'])).toBe(expected);
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: false },
+
+            ['class1', 'class2'],
+        )).toBe(expected);
     });
     test('additional3', () => {
         const expected = 'someClass class1 class2 hovered';
-        expect(classNames('someClass', { hovered: true, scrollable: undefined }, ['class1', 'class2'])).toBe(expected);
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: undefined },
+
+            ['class1', 'class2'],
+        )).toBe(expected);
     });
 });
